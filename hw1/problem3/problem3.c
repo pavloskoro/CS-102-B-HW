d4f3e0363ffd3d7d6b6306a5903a1c0283f278de
Pavlos <pavlos.koronakis@cooper.edu>
2020-09-14 14:16:42 -0400

    problem3

---
+++ hw1/problem3/problem3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+	printf("Quote a \a\n");
+	printf("Quote b \b\n");
+	/* printf("Quote c \c\n"); */
+	/* printf("Quote d \d\n"); */
+	printf("Quote e \e\n");
+	printf("Quote f \f\n");
+	/* printf("Quote g \g\n"); */
+	/* printf("Quote h \h\n"); */
+	/* printf("Quote i \i\n"); */ 
+        /* printf("Quote j \j\n"); */
+        /* printf("Quote k \k\n"); */
+        /* printf("Quote l \l\n"); */
+        /* printf("Quote m \m\n"); */
+        printf("Quote n \n\n");
+        /* printf("Quote o \o\n"); */
+        /* printf("Quote p \p\n"); */
+        /* printf("Quote q \q\n"); */
+        printf("Quote r \r\n");
+        /* printf("Quote s \s\n"); */
+        printf("Quote t \t\n");
+        /* printf("Quote u \u\n"); */
+        printf("Quote v \v\n");
+        /* printf("Quote w \w\n"); */
+        /* printf("Quote x \x\n"); */
+        /* printf("Quote y \y\n"); */
+        /* printf("Quote z \z\n"); */
+   return 0;
+}
---
+++ hw1/problem3/readme.md
@@ -0,0 +1,14 @@
+1. compile program: cc problem3 problem3.c
+2. run program: ./problem3
+3. output:
+
+Quote a 
+Quote b 
+Quote e 
+uote f 
+
+Quote n 
+
+Quote r 
+Quote t 	
+Quote v 
